323ccff204c5d815ae02a739ead67b8142265fd0
prathamesh177 <121346957+prathamesh177@users.noreply.github.com>
2024-09-16 18:14:13 +0530

    Update Store.js

--- backend/models/Store.js
+++ backend/models/Store.js
@@ -1,9 +1,9 @@
 
 
-// module.exports = mongoose.model('Store', storeSchema);
+
 const mongoose = require('mongoose');
 
-// Store Schema
+
 const storeSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -13,19 +13,19 @@ const storeSchema = new mongoose.Schema({
     type: String,
     maxlength: 400,
   },
-  // Array of ratings
+
   ratings: [{
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Reference to User who rated
     rating: { type: Number, min: 1, max: 5 } // Rating between 1 and 5
   }],
-  // Average rating
+
   averageRating: {
     type: Number,
     default: 0,
   },
 });
 
-// Calculate average rating
+
 storeSchema.methods.getAverageRating = function() {
   if (this.ratings.length === 0) return 0; // No ratings yet
   const total = this.ratings.reduce((acc, item) => acc + item.rating, 0); // Sum all ratings
